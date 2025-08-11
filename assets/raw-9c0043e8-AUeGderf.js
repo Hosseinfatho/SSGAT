da7719de99bf2767127aa8b9d0f9a6f22d929e60
Hosseinfathollahian <118398599+Hosseinfathollahian@users.noreply.github.com>
2025-08-11 13:40:02 -0500

    Fix 404 errors by updating index.html to reference correct asset files

--- assets/raw-9c0043e8-AUeGderf.js
+++
@@ -1,1 +0,0 @@
-import{x as s}from"./index-Brbrf8jP.js";class t extends s{decodeBlock(e){return e}}export{t as default};
--- assets/raw-9c0043e8-pGuT6AEi.js
+++
@@ -1,1 +0,0 @@
-import{x as s}from"./index-Bw93sKwH.js";class t extends s{decodeBlock(e){return e}}export{t as default};
